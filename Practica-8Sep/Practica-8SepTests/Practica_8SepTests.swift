c039f1345291d17ea5cf0c4c5f8ee3e568e8cdde
Carlos Encinas <53828095+iCarlosEncinas@users.noreply.github.com>
2021-09-08 17:27:10 -0700

    Add files via upload

---
+++ Practica-8Sep/Practica-8SepTests/Practica_8SepTests.swift
@@ -0,0 +1,33 @@
+//
+//  Practica_8SepTests.swift
+//  Practica-8SepTests
+//
+//  Created by David Encinas on 08/09/21.
+//
+
+import XCTest
+@testable import Practica_8Sep
+
+class Practica_8SepTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
